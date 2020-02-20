c4fa74bf7dce3201b71a3c61bf703fda818aa998
aindusekar <49254314+aindusekar@users.noreply.github.com>
2020-02-20 12:57:06 -0500

    updated to include boxplots and cohens d

--- Technical-Notebook/functions.py
+++ Technical-Notebook/functions.py
@@ -32,3 +32,26 @@ floor2_std = floor2.price.std()
 bathrooms_more_than_2_std = bathrooms_more_than_2.price.std()
 # Standard deviation of price of houses that are less than or equal to 2 bathrooms
 bathrooms_less_and_2_std = bathrooms_less_and_2.price.std()
+
+def Cohen_d(group1, group2):
+
+    # Compute Cohen's d.
+
+    # group1: Series or NumPy array
+    # group2: Series or NumPy array
+
+    # returns a floating point number 
+
+    diff = group1.mean() - group2.mean()
+
+    n1, n2 = len(group1), len(group2)
+    var1 = group1.var()
+    var2 = group2.var()
+
+    # Calculate the pooled threshold as shown earlier
+    pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
+    
+    # Calculate Cohen's d statistic
+    d = diff / np.sqrt(pooled_var)
+    
+    return d
